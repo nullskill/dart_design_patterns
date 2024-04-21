ed39ec4f60401c2beb1b198ca574971681f3a243
nullskill <22912338+nullskill@users.noreply.github.com>
2024-04-21 12:04:03 +0300

    update imports

--- bin/dart_design_patterns.dart
+++ bin/dart_design_patterns.dart
@@ -1,7 +1,9 @@
 import 'package:dart_design_patterns/src/creational/builder.dart' as builder;
+import 'package:dart_design_patterns/src/creational/factory_method.dart' as factory_method;
 import 'package:dart_design_patterns/src/creational/singleton.dart' as singleton;
 
 void main(List<String> arguments) {
   builder.main();
   singleton.main();
+  factory_method.main();
 }
